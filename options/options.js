34908ee655fff47744f0b09cc192a626273d0b7b
Crowdbotics <engineering@crowdbotics.com>
2023-02-04 01:43:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-field-dev-61165",
+  url: "https://winter-field-dev-61165.botics.co",
+  api: "https://winter-field-dev-61165.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
